c8f5578a7089d6affbe0eed5be838cba4b77a5f4
michael.acevedo <michael.acevedo@branderideas.com>
2024-05-03 16:03:07 -0500

    3 comit

--- lib/widgets/add_expenses_wt/bs_num_keyboard.dart
+++ lib/widgets/add_expenses_wt/bs_num_keyboard.dart
@@ -13,6 +13,9 @@ class _BSNumKeyboardState extends State<BSNumKeyboard> {
 
   @override
   Widget build(BuildContext context) {
+    String Function(Match) mathFunc;
+    RegExp reg = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+    mathFunc = (Match match) => '${match[1]},';
     return GestureDetector(
       onTap: () {
         _numPad();
@@ -23,7 +26,7 @@ class _BSNumKeyboardState extends State<BSNumKeyboard> {
           children: [
             const Text('Cantidad Ingresada'),
             Text(
-              '\$ $import',
+              '\$ ${import.replaceAllMapped(reg, mathFunc)}',
               style: const TextStyle(
                   fontSize: 30.0,
                   letterSpacing: 2.0,
@@ -63,86 +66,90 @@ class _BSNumKeyboardState extends State<BSNumKeyboard> {
             borderRadius: BorderRadius.vertical(top: Radius.circular(30.0))),
         context: context,
         builder: (BuildContext context) {
-          return SizedBox(
-            height: 350.0,
-            child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constrains) {
-              var _height = constrains.biggest.height / 5;
-              return Column(
-                children: [
-                  Table(
-                    children: [
-                      TableRow(children: [
-                        _num('1', _height),
-                        _num('2', _height),
-                        _num('3', _height),
-                      ]),
-                      TableRow(children: [
-                        _num('4', _height),
-                        _num('5', _height),
-                        _num('6', _height),
-                      ]),
-                      TableRow(children: [
-                        _num('7', _height),
-                        _num('8', _height),
-                        _num('9', _height),
-                      ]),
-                      TableRow(children: [
-                        _num('.', _height),
-                        _num('0', _height),
-                        GestureDetector(
-                          behavior: HitTestBehavior.opaque,
-                          onTap: () {
-                            setState(() {
-                              if (import.length > 0.0) {
-                                import = import.substring(0, import.length - 1);
-                              }
-                            });
-                          },
-                          onLongPress: () {
-                            setState(() {
-                              import = '';
-                            });
-                          },
-                          child: SizedBox(
-                            height: _height,
-                            child: const Icon(Icons.backspace, size: 35.0),
+          return WillPopScope(
+            onWillPop: () async => false,
+            child: SizedBox(
+              height: 350.0,
+              child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constrains) {
+                var _height = constrains.biggest.height / 5;
+                return Column(
+                  children: [
+                    Table(
+                      children: [
+                        TableRow(children: [
+                          _num('1', _height),
+                          _num('2', _height),
+                          _num('3', _height),
+                        ]),
+                        TableRow(children: [
+                          _num('4', _height),
+                          _num('5', _height),
+                          _num('6', _height),
+                        ]),
+                        TableRow(children: [
+                          _num('7', _height),
+                          _num('8', _height),
+                          _num('9', _height),
+                        ]),
+                        TableRow(children: [
+                          _num('.', _height),
+                          _num('0', _height),
+                          GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () {
+                              setState(() {
+                                if (import.length > 0.0) {
+                                  import =
+                                      import.substring(0, import.length - 1);
+                                }
+                              });
+                            },
+                            onLongPress: () {
+                              setState(() {
+                                import = '';
+                              });
+                            },
+                            child: SizedBox(
+                              height: _height,
+                              child: const Icon(Icons.backspace, size: 35.0),
+                            ),
+                          )
+                        ])
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            child: Constants.customButton(
+                                Colors.transparent, Colors.red, 'CANCELAR'),
+                            onTap: () {
+                              setState(() {
+                                import = '0.00';
+                                Navigator.pop(context);
+                              });
+                            },
                           ),
-                        )
-                      ])
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          child: Constants.customButton(
-                              Colors.transparent, Colors.red, 'CANCELAR'),
-                          onTap: () {
-                            setState(() {
-                              import = '0.00';
-                              Navigator.pop(context);
-                            });
-                          },
                         ),
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          child: Constants.customButton(
-                              Colors.green, Colors.transparent, 'ACEPTAR'),
-                          onTap: () {
-                            setState(() {
-                              if (import.length == 0.0) import = '0.00';
-                              Navigator.pop(context);
-                            });
-                          },
+                        Expanded(
+                          child: GestureDetector(
+                            child: Constants.customButton(
+                                Colors.green, Colors.transparent, 'ACEPTAR'),
+                            onTap: () {
+                              setState(() {
+                                if (import.length == 0.0) import = '0.00';
+                                Navigator.pop(context);
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              );
-            }),
+                      ],
+                    )
+                  ],
+                );
+              }),
+            ),
           );
         });
   }
